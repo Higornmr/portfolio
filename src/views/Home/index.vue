ca970250b2f578905e2278042e115d43a42eeb59
HigorMRodrigues <higor@planmkt.com.br>
2024-01-20 13:34:58 -0300

    fix: Adicionado imagens

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="border-red-950 w-full border-solid border-4 py-4">
-    <div class="flex flex-col items-center">
-      <h1 class="uppercase">
+    <div class="flex flex-col items-center py-3">
+      <h1 class="font-bold text-7xl">
         Crie seu website !
       </h1>
 
-      <h2 class="">
+      <h2 class="text-5xl">
         Sua landingpage
       </h2>
     </div>
--- src/views/Home/sections/IntroducaoSection.vue
+++ src/views/Home/sections/IntroducaoSection.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full flex flex-col justify-center items-center relative">
     <div class="bg-[url('/images/mobile.webp')] bg-cover w-[241px] h-[474px]"></div>
-    <div class="w-full sm:max-w-[510px] max-w-[320px] pl-3 pr-2 absolute">
+    <div class="w-full sm:max-w-[510px] max-w-[320px] pl-3 pr-2 absolute bottom-14">
       <SliderExample />
     </div>
   </div>
